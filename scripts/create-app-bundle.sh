3617dd105453f0a5cb400c18f36970a6a1934813
Nikki Ng <ng.niccc@gmail.com>
2021-04-20 17:21:34 +0800

    formatting

--- scripts/create-app-bundle.sh
+++ scripts/create-app-bundle.sh
@@ -7,9 +7,9 @@ cd ./tmp
 cp ../docker-compose.production.yml docker-compose.yml
 zip -r $(git rev-parse HEAD).zip docker-compose.yml 
 aws s3 cp $(git rev-parse HEAD).zip s3://elasticbeanstalk-ap-southeast-1-461960015384/$(git rev-parse HEAD).zip 
-aws elasticbeanstalk create-application-version \            
-	--application-name main \            
-	--version-label "$(git rev-parse HEAD)" \            
-	--description "$(git log -1 --pretty=%s | cut -c -200)" \            
-	--source-bundle S3Bucket="elasticbeanstalk-ap-southeast-1-461960015384",S3Key="$(git rev-parse HEAD).zip
+aws elasticbeanstalk create-application-version \
+    --application-name "main" \
+    --version-label "$(git rev-parse HEAD)" \
+    --description "$(git log -1 --pretty=%s | cut -c -200)" \
+    --source-bundle S3Bucket="elasticbeanstalk-ap-southeast-1-461960015384",S3Key="$(git rev-parse HEAD).zip"
 cd .. && rm -rf ./tmp
